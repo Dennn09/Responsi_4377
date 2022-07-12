de07837fffa79913e883e711e4c9f086600ce39e
Dennn09 <97225549+Dennn09@users.noreply.github.com>
2022-07-12 13:17:44 +0800

    Responsi_4377
    Produk Penjualan

---
+++ responsi2/responsi2/Produk.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsiuas2377
+{
+    class Produk
+    {
+        public string Nota { get; set; }
+        public string tanggal { get; set; }
+        public string Custumer { get; set; }
+        public string Jenis { get; set; }
+        public string TotalNota { get; set; }
+
+    }
+}
